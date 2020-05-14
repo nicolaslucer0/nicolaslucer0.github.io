710e3197a07cb206f5e8fe66753998334adcfee3
Nicolas <nlucero@glamit.com.ar>
2020-05-14 13:15:16 -0300

    html puro

---
+++ precache-manifest.dd78f788c59910736f7ba90fb7ceec4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dc679b279d4d8b586bcc37e85ab149",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1d407d24f77523dd6f0",
+    "url": "/static/css/main.b93a33fc.chunk.css"
+  },
+  {
+    "revision": "69b108489ed7a09dd487",
+    "url": "/static/js/2.f18027a7.chunk.js"
+  },
+  {
+    "revision": "430cff1616a0a7515add39dc426f62cc",
+    "url": "/static/js/2.f18027a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d407d24f77523dd6f0",
+    "url": "/static/js/main.b665960b.chunk.js"
+  },
+  {
+    "revision": "a507ee7e257c590e358c",
+    "url": "/static/js/runtime-main.b918eca6.js"
+  },
+  {
+    "revision": "654ce14c48c382a375b02ca71dba628e",
+    "url": "/static/media/header.654ce14c.jpg"
+  }
+]);
